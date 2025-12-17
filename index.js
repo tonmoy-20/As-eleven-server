b35e7df4c42dbe0beace5ddcf5c58514d4cc3c9f
Tonmoy <tonmoybiswas20602@gmail.com>
2025-12-18 01:44:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const admin = require("firebase-admin");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+  try {
+    const idToken = token.split("")[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    console.log("decoded info", decoded);
+    req.decoded_email = decoded.email;
+  } catch (error) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+};
+
+const uri =
+  "mongodb+srv://missionscic11:I6r3E9StM4MVkJLw@cluster0.tzswtx3.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const dataBase = client.db("missionscic11DB");
+    const userCollections = dataBase.collection("user");
+    const ProductCollections = dataBase.collection("product");
+    const requestCollections = dataBase.collection("request");
+
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      userInfo.role = "donor";
+      userInfo.status = "active";
+      userInfo.createdAt = new Date();
+      const result = await userCollections.insertOne(userInfo);
+      res.send(result);
+    });
+
+    app.get(`/users/role/:email`, async (req, res) => {
+      const { email } = req.params;
+      const query = { email: email };
+      const result = await userCollections.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
+    // request
+
+    app.post("/requests", verifyToken, async (req, res) => {
+      const data = req.body;
+      data.createdAt = new Date();
+      const result = await requestCollections.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/manager/products/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { managerEmail: email };
+
+      const result = await ProductCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello, Mission SCIC ");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
